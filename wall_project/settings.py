99108d451b710d28537f88a701d24511abb33de1
Monica Hong <M0M0-D3V@users.noreply.github.com>
2020-04-18 13:56:43 -0700

    linking login and wall apps to project

--- wall_project/settings.py
+++ wall_project/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'login_app',
+    'wall_app',
+
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
